931769ffd7072823a859d2424ed89ce3e156ce6c
Adrian Ochmann <adrian.ochmann@outlook.com>
2022-12-01 22:05:17 +0100

    feat: upgraded packages to net7 and Umbraco 11

--- src/FeatureManagement.ExampleWeb/Program.cs
+++ src/FeatureManagement.ExampleWeb/Program.cs
@@ -1,61 +1,19 @@
-﻿using FeatureManagement.ExampleWeb;
-using FeaturesManagementDashboard.Extensions;
-using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Extensions;
-
-var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureLogging(configure => configure.ClearProviders())
-    .ConfigureWebHostDefaults(webHost => webHost.UseStartup<Startup>());
-
-using var app = builder.Build();
-
-await app.RunAsync();
-
 namespace FeatureManagement.ExampleWeb
 {
-#pragma warning disable SA1649 // File name should match first type name
-
-    internal class Startup
+    public static class Program
     {
-        private readonly IWebHostEnvironment _env;
-        private readonly IConfiguration _config;
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
 
-        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
-        {
-            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
-            _config = config ?? throw new ArgumentNullException(nameof(config));
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-            => services
-                .AddUmbraco(_env, _config)
-                .AddBackOffice()
-                .AddWebsite()
-                .AddComposers()
-                .AddFeaturesManagementDashboard()
-                .Build();
-
-        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseUmbraco()
-                .WithMiddleware(u =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    u.UseBackOffice();
-                    u.UseWebsite();
-                })
-                .WithEndpoints(u =>
-                {
-                    u.UseInstallerEndpoints();
-                    u.UseBackOfficeEndpoints();
-                    u.UseWebsiteEndpoints();
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
                 });
-        }
     }
-
-#pragma warning restore SA1649 // File name should match first type name
 }
