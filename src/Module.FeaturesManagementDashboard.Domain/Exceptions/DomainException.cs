4fd86031bc1186a15d1c33a98bb7c91520528804
Adrian Ochmann <adrian.ochmann@outlook.com>
2023-10-03 08:25:56 +0200

    refactor: added global project configuration

--- src/Module.FeaturesManagementDashboard.Domain/Exceptions/DomainException.cs
+++ src/Module.FeaturesManagementDashboard.Domain/Exceptions/DomainException.cs
@@ -2,7 +2,7 @@
 {
     public abstract class DomainException : Exception
     {
-        public virtual string Code { get; }
+        public virtual string Code { get; } = string.Empty;
 
         protected DomainException(string message) : base(message)
         {
